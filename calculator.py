0154d765cd455b5dee89b9f89e6adafb00d0a469
paayaw521 <yaw.sobreh@turntabl.io>
2025-06-16 09:26:15 +0000

    changes 16 june

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
+import unittest
+
 def add(a, b):
     return a + b
 
@@ -13,3 +15,7 @@ def subtract(a,b):
 
 def divide(a,b):
     return a/b
+
+
+
+    
--- testFile.py
+++ testFile.py
@@ -1,3 +1,5 @@
+from calculator import add, multiply, subtract, divide
+
 
 def test_add():
     assert add(2, 3) == 5
